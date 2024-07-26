9d2893ddabf2d3b445fbce5068b5ee38cafbbc66
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2024-07-26 10:23:23 +0200

    Real chat example working

--- client.py
+++ client.py
@@ -1,27 +1,49 @@
 import asyncio
 import socketio
+import sys
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.patch_stdout import patch_stdout
 
 CHAT_NAMESPACE = "/chat"
+EVENT = "chat_message"
+REGISTER = "chat_register"
+NAME = sys.argv[1]
 
 sio = socketio.AsyncClient()
 chat_event = sio.event(namespace=CHAT_NAMESPACE)
 
+async def send_message(message):
+    await sio.emit(EVENT, message, namespace=CHAT_NAMESPACE)
+
+async def register(name):
+    await sio.emit(REGISTER, name, namespace=CHAT_NAMESPACE)
 
 @chat_event
 async def connect():
     print('connection established')
+    await register(NAME)
 
 @chat_event
-async def chat_message(data):
-    print('message received with ', data)
+async def chat_message(message):
+    print(message)
 
 @chat_event
 async def disconnect():
     print('disconnected from server')
 
+async def interactive_shell():
+    session = PromptSession("Say something: ")
+    while True:
+        message = await session.prompt_async()
+        await send_message(message)
+
 async def main():
-    await sio.connect('http://localhost:8080', namespaces=[CHAT_NAMESPACE])
-    await sio.wait()
+    with patch_stdout():
+        await sio.connect('http://localhost:8080', namespaces=[CHAT_NAMESPACE])
+        shell = asyncio.create_task(interactive_shell())
+        await sio.wait()
+    shell.cancel()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- serv.py
+++ serv.py
@@ -2,32 +2,43 @@ import aiohttp.web
 import socketio
 
 CHAT_NAMESPACE = "/chat"
+CHAT_ROOM = "chat_room"
+EVENT = "chat_message"
 
 app = aiohttp.web.Application()
 sio = socketio.AsyncServer(namespaces=[CHAT_NAMESPACE])
 sio.attach(app)
 chat_event = sio.event(namespace=CHAT_NAMESPACE)
 
+USERS = {}
+
+async def send_message(message):
+    await sio.emit(EVENT, message, namespace=CHAT_NAMESPACE, room=CHAT_ROOM)
 
 @chat_event
 async def connect(sid, environ):
-    print("connect ", sid)
-    await sio.enter_room(sid, "chat_room", namespace=CHAT_NAMESPACE)
+    print(f"{sid} connected")
 
 @chat_event
-async def chat_message(sid, data):
-    print("message ", data)
-    await sio.emit(
-        "chat_message", 
-        data,
-        namespace="/chat",
-        room="chat_room"
-    )
+async def chat_register(sid, name):
+    print(f"{sid} registered as {name}")
+    USERS[sid] = name
+    await sio.enter_room(sid, "chat_room", namespace=CHAT_NAMESPACE)
+    await send_message(f"{name} entered chat")
+
+@chat_event
+async def chat_message(sid, message):
+    name = USERS.get(sid)
+    if name is None:
+        return
+    await send_message(f"{name}: {message}")
 
 @chat_event
 async def disconnect(sid):
-    print('disconnect ', sid)
-    await sio.leave_room(sid, "chat_room", namespace=CHAT_NAMESPACE)
+    name = USERS.pop(sid, None)
+    print("{sid} ({name}) disconnected")
+    if name:
+        await sio.leave_room(sid, "chat_room", namespace=CHAT_NAMESPACE)
 
 if __name__ == '__main__':
     aiohttp.web.run_app(app)
